690993777d88a7b813726cab28ffb12d5cc5c118
Sam Keim <skeim235@gmail.com>
2020-07-01 12:37:37 -0400

    First commit, project complete

--- src/string_methods/MyStringMethodsTest.java
+++ src/string_methods/MyStringMethodsTest.java
@@ -5,17 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MyStringMethodsTest {
+	private static MyStringMethods msm = new MyStringMethods();
 	// COUNT OCCURRENCES STRING
 	@Test
 	void testCountOccurrences() {
-		MyStringMethods msm = new MyStringMethods();
 		msm.setString("Apples Apples Apples");
 		assertEquals(3,msm.countOccurrences("Appl"));
 	}
 
 	@Test
 	void testCountOccurrencesBig() {
-		MyStringMethods msm = new MyStringMethods();
 		msm.setString("Aardvarks get big and bigger as You feed them big and bigger\r\n" + 
 				"ants who Feed on apples");
 		assertEquals(4, msm.countOccurrences("big"));
@@ -24,14 +23,12 @@ class MyStringMethodsTest {
 	// COUNT OCCURRENCES CHAR
 	@Test
 	void testCountOccurrencesCharA() {
-		MyStringMethods msm = new MyStringMethods();
 		msm.setString("Apples Apples Apples");
 		assertEquals(3,msm.countOccurrences('A'));
 	}
 
 	@Test
 	void testCountOccurrencesCharB() {
-		MyStringMethods msm = new MyStringMethods();
 		msm.setString("Aardvarks get big and bigger as You feed them big and bigger\r\n" + 
 				"ants who Feed on apples");
 		assertEquals(7, msm.countOccurrences('a'));
@@ -40,14 +37,12 @@ class MyStringMethodsTest {
 	// COUNT UPPERCASE LETTERS
 	@Test
 	void testCountUpperCaseLetters() {
-		MyStringMethods msm = new MyStringMethods();
 		msm.setString("Apples Apples Apples");
 		assertEquals(3,msm.countUpperCaseLetters());
 	}
 
 	@Test
 	void testCountUpperCaseLettersB() {
-		MyStringMethods msm = new MyStringMethods();
 		msm.setString("Aardvarks get big and bigger as You feed them big and bigger\r\n" + 
 				"ants who Feed on apples");
 		assertEquals(3, msm.countUpperCaseLetters());
@@ -55,14 +50,12 @@ class MyStringMethodsTest {
 	// COUNT LOWERCASE LETTERS
 	@Test
 	void testCountLowerCaseLetters() {
-		MyStringMethods msm = new MyStringMethods();
 		msm.setString("Apples Apples Apples");
 		assertEquals(15,msm.countLowerCaseLetters());
 	}
 
 	@Test
 	void testCountLowerCaseLettersB() {
-		MyStringMethods msm = new MyStringMethods();
 		msm.setString("Aardvarks get big and bigger as You feed them big and bigger\r\n" + 
 				"ants who Feed on apples");
 		assertEquals(65, msm.countLowerCaseLetters());
